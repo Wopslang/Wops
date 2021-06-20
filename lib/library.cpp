83c1c69c1d98902d066ba911b524284ee7d2ff85
codenstory <codenstory@protonmail.ch>
2021-06-20 21:29:20 +0900

    Translate library comments

--- lib/library.cpp
+++ lib/library.cpp
@@ -9,45 +9,44 @@
 #include <string>
 #include "../src/type/variable.h"
 
-// 표준 I/O 함수
+// Standard I/O Functions
 
 /**
-* @brief out(any s): 표준 출력
-* 함수 정보
-* 이름: out
-* 매개변수: (any s)
-* 반환형: none
+* @brief out(any s): Standard output
+* @name out
+* @param (any s)
+* @return none
 */
-extern "C" VariableWithCode out(Variable s) { 
+extern "C" VariableWithCode out(Variable s) {
     // EMPTY VARIABLE
     Variable null = Variable("_", "", INT);
-    
+
     std::cout << s.GetValue();
     return {null, OK};
 }
 
 /**
-* @brief string in(): 표준 입력
-* 이름: in
-* 매개변수: none
-* 반환형: string s
-* 띄어쓰기 혹은 줄바꿈 시 입력이 종료됩니다
+* @brief string in(): Standard input
+* @name in
+* @param none
+* @return string s
+* Terminates inputting when space or new line character is entered
 */
-extern "C" VariableWithCode in(Variable _) { 
+extern "C" VariableWithCode in(Variable _) {
     std::string s; std::cin >> s;
     Variable ret = Variable("_", s, STRING);
     return {ret, OK};
 }
 
-// 수학 함수
+// Mathematics Functions
 
 /**
- * @brief int phi(int a): a에 대한 오일러 파이값
- * 이름: phi
- * 매개변수: int a
- * 반환형: int a
+ * @brief int phi(int a): Euler's totient of a
+ * @name phi
+ * @param int a
+ * @return int a
  */
-extern "C" VariableWithCode phi(Variable a) { 
+extern "C" VariableWithCode phi(Variable a) {
     Variable err("_", "", INT);
     if (a._t != INT) return {err, ERROR};
 
@@ -61,7 +60,7 @@ extern "C" VariableWithCode phi(Variable a) {
             while (!(n % i)) n /= i;
         }
     }
-    
+
     ret = (n > 1 ? ret - (ret / n) : ret);
 
     Variable res("_", std::to_string(ret), INT);
